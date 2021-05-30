15672a72c71a7e2490d7d9be2f97dcb468551e09
vaishnaviThakare <vaishnavithakre03@gmail.com>
2021-05-30 21:21:45 +0530

    updated bot

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut, AmplifyChatbot } from '@aws-amplify/ui-react';
+import { Interactions } from 'aws-amplify';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AmplifySignOut />
+
+       <AmplifyChatbot
+    botName="OrderFlowers_dev"
+    botTitle="My ChatBot"
+    welcomeMessage="Hello, how can I help you?"
+  />
+
+     
     </div>
   );
 }
 
-export default App;
+
+
+export default withAuthenticator(App);
